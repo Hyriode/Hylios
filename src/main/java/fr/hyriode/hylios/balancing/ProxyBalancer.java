9525674d9ca9c7a4d929ed4289c9f5a55cd7e0e2
AstFaster <astfaster@gmail.com>
2023-04-24 17:05:45 +0200

    Remove debug

--- src/main/java/fr/hyriode/hylios/balancing/ProxyBalancer.java
+++ src/main/java/fr/hyriode/hylios/balancing/ProxyBalancer.java
@@ -32,9 +32,6 @@ public class ProxyBalancer {
 
         int neededProxies = (int) (Math.ceil((double) players * 1.5 / HyggProxy.MAX_PLAYERS));
 
-        System.out.println(neededProxies);
-        System.out.println(minProxies);
-
         if (neededProxies < minProxies) {
             neededProxies = minProxies;
         }
